8440ee7ca3bbd01ffa1772610ccb81b3c996ab5d
Pavel Zaitsev <pavel@arslogi.ca>
2019-03-30 12:20:00 +0300

    Initial Commit

---
+++ instapicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instapicture
+//
+//  Created by Pavel Zaitsev on 3/30/19.
+//  Copyright © 2019 Pavel Zaitsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
