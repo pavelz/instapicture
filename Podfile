75458073e683248001b68e782a1c8241bb4ba0ab
Pavel Zaitsev <pavel@arslogi.ca>
2019-03-31 19:05:33 +0300

    code

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.0'
+use_frameworks!
+
+target 'instapicture' do
+  pod 'AFNetworking'
+  pod 'ORStackView'
+  pod 'SwiftyJSON'
+end
--- instapicture/ViewController.swift
+++ instapicture/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var selectPhoto: UIButton!
+    @IBOutlet var takePhoto: UIButton!
+    @IBOutlet var ImageView: UIImageView!
+    
+    @IBOutlet var SendImage: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func takePhoto(button: UIButton){
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let picker: UIImagePickerController = UIImagePickerController()
 
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = .camera
+            self.present(picker, animated:true, completion: nil)
 
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            ImageView.contentMode = .scaleToFill
+            ImageView.image = pickedImage
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+     }
+    
+    @IBAction func SendImage(button: UIButton){
+        
+    }
+    
 }
 
